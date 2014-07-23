308628c43168e687bea6f8799cf0b5165fa0da29
dtgillis <dtgillis@renci.org>
2014-07-23 17:46:57 -0400

    Initial commit for CNV data database
    Initial experiment data structures set up except for software

---
+++ experiment/data.py
@@ -0,0 +1,79 @@
+__author__ = 'dtgillis'
+
+import os; os.environ['DJANGO_SETTINGS_MODULE'] = 'cnv_data.settings'; import django
+from experiment import models as exp
+
+
+def create_sample_from_mapping(sample_line, xt2):
+
+    line = sample_line.strip(os.linesep)
+    fields = line.split()
+    sample_name = fields[0]
+    bam_file = fields[1]
+    sample = exp.Sample.objects.create_sample_model(sample_name, bam_file, xt2)
+    sample.save()
+
+
+def create_chrm_record(chrm_line):
+
+    fields = chrm_line.strip(os.linesep).split()
+    chrom_name = fields[0]
+    chrm_alt = fields[1]
+    chromosome = exp.Chromosome.objects.create_chromosome(chrom_name, chrm_alt)
+    chromosome.save()
+
+
+def create_interval_record(interval_line, chrm_obj):
+
+    fields = interval_line.strip(os.linesep).split(":")
+    chrm_name = fields[0]
+    if str(chrm_obj.chromosome_name) != chrm_name:
+        chrm_obj = exp.Chromosome.objects.filter(chromosome_name=chrm_name).get()
+    start = int(fields[1].split("-")[0])
+    end = int(fields[1].split("-")[1])
+    interval = exp.Interval(chromosome=chrm_obj, start=start, end=end)
+    return interval, chrm_obj
+
+
+def build_data(base_dir):
+    #different file names
+    xt2_sample_list = 'xt2_mapping.dat'
+    old_bam_list = 'old_bam_map.dat'
+    chrm_list_file = 'chrm_list'
+    interval_list_file = 'agilent.interval_list'
+    ## load sample data into database
+
+    # xt2 samples
+    for line in open(base_dir + os.sep + 'sample' + os.sep + xt2_sample_list, 'r'):
+        if len(line) > 0:
+            create_sample_from_mapping(line, True)
+
+    for line in open(base_dir + os.sep + 'sample' + os.sep + old_bam_list, 'r'):
+        if len(line) > 0:
+            create_sample_from_mapping(line, False)
+
+    # load up chromosome table
+
+    for line in open(base_dir + os.sep + 'interval' + os.sep + chrm_list_file):
+        if len(line) > 0:
+            create_chrm_record(line)
+
+    interval_list = []
+    chrm_obj = exp.Chromosome.objects.filter(chromosome_name='NC_000001.10').get()
+    for line in open(base_dir + os.sep + 'interval' + os.sep + interval_list_file):
+
+        if len(line) > 0:
+            interval, chrm_obj = create_interval_record(line, chrm_obj)
+            interval_list.append(interval)
+
+    exp.Interval.objects.bulk_create(interval_list)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    build_data('/home/dtgillis/sim_capture/django_data')
---
+++ experiment/models.py
@@ -0,0 +1,98 @@
+from django.db import models
+
+
+class SampleManager(models.Manager):
+
+    def create_sample_model(self, sample_name, bam_file, xt2_sample):
+        sample = self.create(sample_name=sample_name, bam_file=bam_file, xt2_sample=xt2_sample)
+        return sample
+
+
+class Sample(models.Model):
+
+    sample_name = models.CharField(max_length=100)
+    bam_file = models.CharField(max_length=200)
+    xt2_sample = models.BooleanField()
+    objects = SampleManager()
+
+    class Meta:
+        unique_together = ('sample_name', 'bam_file')
+
+    def __unicode__(self):
+        if not self.xt2_sample:
+            return self.sample_name + '-non-xt2'
+        else:
+            return self.sample_name + '-xt2'
+
+
+class PoolManager(models.Manager):
+
+    def create_pool(self, sample, pool_name):
+        pool = self.create(sample=sample, pool_name=pool_name)
+        return pool
+
+
+class Pool(models.Model):
+
+    sample = models.ForeignKey(Sample)
+    pool_name = models.CharField(max_length=100)
+    objects = PoolManager()
+
+    def __unicode__(self):
+        return self.pool_name
+
+
+class SoftwareManager(models.Manager):
+
+    def create_software(self, software_name, extra_params):
+        software = self.create(software_name=software_name, extra_params=extra_params)
+        return software
+
+
+class Software(models.Model):
+
+    software_name = models.CharField(max_length=50)
+    extra_params = models.TextField()
+    objects = SoftwareManager()
+
+    def __unicode__(self):
+        return self.software_name + ":" + self.extra_params
+
+
+class ChromosomeManager(models.Manager):
+
+    def create_chromosome(self, chromosome_name, alternate_name):
+        chromosome = self.create(chromosome_name=chromosome_name, alternate_name=alternate_name)
+        return chromosome
+
+
+class Chromosome(models.Model):
+
+    chromosome_name = models.CharField(max_length=25)
+    alternate_name = models.CharField(max_length=10)
+    objects = ChromosomeManager()
+
+    def __unicode__(self):
+        return self.chromosome_name
+
+
+class IntervalManager(models.Manager):
+
+    def create_interval(self, chromosome, start, end):
+        interval = self.create(chromosome=chromosome, start=start, end=end)
+        return interval
+
+
+class Interval(models.Model):
+
+    chromosome = models.ForeignKey(Chromosome)
+    start = models.PositiveIntegerField()
+    end = models.PositiveIntegerField()
+    objects = IntervalManager()
+
+    class Meta:
+        unique_together = ('chromosome', 'start', 'end')
+
+    def __unicode__(self):
+
+        return "{0:s}:{1:d}-{2:d)".format(self.chromosome.__unicode__(), self.start, self.end)
